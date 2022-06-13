ce58f38ddf264e1acc496a44378d0f509722ceb8
mark <mark@10.211.55.3>
2022-06-13 14:48:40 -0400

    Added column titles to EventsMenu

--- src/bark/EventsMenu.java
+++ src/bark/EventsMenu.java
@@ -8,7 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.util.Date; 
 import tables.Event;
+import tables.EventHistory;
 
 public class EventsMenu extends Login1 {
     Home home;
@@ -17,6 +20,23 @@ public class EventsMenu extends Login1 {
     TableView<Event> eventTable2 = new TableView<>();
     TableView<Event> eventTable3 = new TableView<>();
     ObservableList<Event> tableData = FXCollections.observableArrayList();
+    
+    //Create the columsn for each table
+    TableColumn dailyEvent = new TableColumn("Event");
+    TableColumn dailyDate = new TableColumn("Event Date");
+    TableColumn dailyDuration = new TableColumn("Event Duration");
+    
+    TableColumn pastEvent = new TableColumn("Event");
+    TableColumn pastMax = new TableColumn("Max Volunteers");
+    TableColumn pastAssigned = new TableColumn("Assigned Volunteers");
+    TableColumn pastDate = new TableColumn("Event Date");
+    
+    TableColumn yourName = new TableColumn("Event Name"); 
+    TableColumn yourDate = new TableColumn("Event Date");
+    TableColumn yourDuration = new TableColumn("Duration");
+    TableColumn yourDistance = new TableColumn("Distance");
+    TableColumn yourCategory = new TableColumn("Category");
+     
     
     // Tab 1 Controls
     
@@ -55,6 +75,29 @@ public class EventsMenu extends Login1 {
         tPane1.add(eventTable, 0, 0);
         tPane2.add(eventTable2, 0, 0);
         tPane3.add(eventTable3, 0, 0);
+        
+        
+        //Set the cell values for eventTable (Daily Events)
+        dailyEvent.setCellValueFactory(new PropertyValueFactory<Event, String>("eventName"));
+        dailyDate.setCellValueFactory(new PropertyValueFactory<Event, Date>("eventDate"));
+        dailyDuration.setCellValueFactory(new PropertyValueFactory<Event, String>("eventTime"));
+        
+        pastEvent.setCellValueFactory(new PropertyValueFactory<Event, String>("eventName"));
+        pastMax.setCellValueFactory(new PropertyValueFactory<Event, Integer>("maxVolunteers"));
+        //pastAssigned.setCellValueFactory(new PropertyValueFactory<Event, String>("eventName"));
+        pastDate.setCellValueFactory(new PropertyValueFactory<Event, Date>("eventDate"));
+        
+        
+        yourName.setCellValueFactory(new PropertyValueFactory<Event, String>("eventName"));
+        yourDate.setCellValueFactory(new PropertyValueFactory<Event, Date>("eventDate"));
+        yourDuration.setCellValueFactory(new PropertyValueFactory<Event, String>("eventTime"));
+        yourDistance.setCellValueFactory(new PropertyValueFactory<EventHistory, Integer>("miles_Driven"));
+        yourCategory.setCellValueFactory(new PropertyValueFactory<Event, String>("eventType"));
+
+        eventTable.getColumns().addAll(dailyEvent, dailyDate, dailyDuration); 
+        eventTable2.getColumns().addAll(pastEvent, pastMax, pastDate); 
+        eventTable3.getColumns().addAll(yourName, yourDate, yourDuration, yourDistance, yourCategory); 
+        
         
         Stage primaryStage = new Stage();
         Scene primaryScene = new Scene(eventsPane, 600, 450);
