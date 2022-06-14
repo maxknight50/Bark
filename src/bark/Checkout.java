9bbe26ae4d9d3ba30fc3eee6bd929528c3f8afc5
joshr <joshr@DESKTOP-PIVQ7KF>
2022-06-14 16:20:13 -0400

    Checkout - back button, takes home scene

--- src/bark/Checkout.java
+++ src/bark/Checkout.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 public class Checkout extends Login1 {
     Home home;
     
-    Button backLbl = new Button("Back");
+    Button backBtn = new Button("Back");
     Label titleLbl = new Label("Check Out");
     Label currentHrsLbl = new Label("Your current hours: ");
     TextField currentHrsTxt = new TextField();
@@ -25,35 +25,32 @@ public class Checkout extends Login1 {
     TextField volIdTxt = new TextField();
     Button checkoutBtn = new Button("CHECK OUT");
     
-    
     GridPane checkoutPane = new GridPane();
-    
     
     Checkout(Home home) {
         this.home = home;
         paneSettings(checkoutPane);
         
         // add nodes
-        //checkoutPane.add(backLbl, 0, 0);
-        checkoutPane.add(titleLbl, 0, 0);
-        checkoutPane.add(currentHrsLbl, 0, 1);
-        checkoutPane.add(currentHrsTxt, 1, 1);
-        checkoutPane.add(usernameLbl, 0, 2);
-        checkoutPane.add(usernameTxt, 0, 3);
-        checkoutPane.add(passwordLbl, 0, 4);
-        checkoutPane.add(passwordTxt, 0, 5);
-        checkoutPane.add(volIdLbl, 0, 6);
-        checkoutPane.add(volIdTxt, 0, 7);
-        checkoutPane.add(checkoutBtn, 1, 8);
+        checkoutPane.add(backBtn, 0, 0);
+        checkoutPane.add(titleLbl, 1, 0);
+        checkoutPane.add(currentHrsLbl, 1, 1);
+        checkoutPane.add(currentHrsTxt, 2, 1);
+        checkoutPane.add(usernameLbl, 1, 2);
+        checkoutPane.add(usernameTxt, 1, 3);
+        checkoutPane.add(passwordLbl, 1, 4);
+        checkoutPane.add(passwordTxt, 1, 5);
+        checkoutPane.add(volIdLbl, 1, 6);
+        checkoutPane.add(volIdTxt, 1, 7);
+        checkoutPane.add(checkoutBtn, 2, 8);
         
-
-        Stage primaryStage = new Stage();
+        Stage primaryStage = home.primaryStage;
         Scene primaryScene = new Scene(checkoutPane, 600, 450);
         primaryStage.setScene(primaryScene);
         primaryStage.setTitle("BARK Checkout");
         primaryStage.show();
         
-        backLbl.setOnAction(e -> {
+        backBtn.setOnAction(e -> {
             primaryStage.close(); 
         });
     }
