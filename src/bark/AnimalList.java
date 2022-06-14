1b747d4a623eba8b11f1ea0b749c9eea38fbb20e
mark <mark@10.211.55.3>
2022-06-14 16:51:15 -0400

    Animal History Table Population

--- src/bark/AnimalList.java
+++ src/bark/AnimalList.java
@@ -29,8 +29,8 @@ public class AnimalList extends Application {
     static Connection conn;
     ResultSet rs;
 
-    TableView<ListVolunteers> volTable = new TableView<>();
-    ObservableList<ListVolunteers> tableData = FXCollections.observableArrayList();
+    TableView<Animal> volTable = new TableView<>();
+    ObservableList<Animal> tableData = FXCollections.observableArrayList();
 
     TableColumn id_col = new TableColumn("animalID");
     TableColumn name_col = new TableColumn("name");
@@ -61,19 +61,18 @@ public class AnimalList extends Application {
         
         volTable.getColumns().addAll(id_col, name_col, species_col, age_col, history_col, feeding_col, vethist_col, event_col, volID_col);
         
-        sendDBCommand("SELECT animal_ID, name, species, age, medicalHistory, feedingNeeds, vetHistory, eventID, volID FROM Animal");
-        System.out.println("RESULTSET: " + rs);
+        sendDBCommand("SELECT animal_ID, name, species, age, medicalHistory, feedingNeeds, vetHistory, event_ID, volID FROM Animal");
         try {
-            ListVolunteers[] volunteerList = new ListVolunteers[25];
+            Animal[] animalList = new Animal[25];
             for(int i = 0; i < 100; i++){
                 while(rs.next()){
                     if (rs != null){
-                        volunteerList[i] = new ListVolunteers(rs.getInt("volID"), rs.getString("vol_FirstName"), rs.getString("vol_LastName"), rs.getString("vol_Address"),rs.getDate("vol_DateOfBirth"), rs.getString("vol_Email"), rs.getString("vol_Phone"), rs.getDouble("cumulativeHours"), rs.getString("status"));
+                        animalList[i] = new Animal(rs.getInt("animal_ID"), rs.getString("name"), rs.getString("species"), rs.getInt("age"), rs.getString("medicalHistory"), rs.getString("feedingNeeds"), rs.getString("vetHistory"), rs.getInt("event_ID"), rs.getInt("volID"));
                         break;
                     }
                 }
             }
-            for(ListVolunteers x : volunteerList){
+            for(Animal x : animalList){
                 tableData.add(x); 
             }
             
@@ -106,7 +105,6 @@ public class AnimalList extends Application {
             conn = ds.getConnection(userID, userPASS);
             stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
             rs = stmt.executeQuery(sqlQuery); // Sends the Query to the DB
-            System.out.println("RESULT SET: " + rs);
 
         } catch (SQLException e) {
             System.out.println(e.toString());
