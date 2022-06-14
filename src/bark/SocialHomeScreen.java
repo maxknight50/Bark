749bbced525e1732e24d491464fbd4f47b9c6ad4
joshr <joshr@DESKTOP-PIVQ7KF>
2022-06-13 20:59:00 -0400

    create social home screen

---
+++ src/bark/SocialHomeScreen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bark;
+
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * 
+ */
+public class SocialHomeScreen extends Login1 {
+    Home home;
+    
+    GridPane socialPane = new GridPane();
+}
