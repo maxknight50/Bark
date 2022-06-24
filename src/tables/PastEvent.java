659163c31f23dac875def87c5f9269bb9570bd3b
maxknight50 <maxknight50@gmail.com>
2022-06-24 17:45:06 -0400

    Changed table size

--- src/tables/PastEvent.java
+++ src/tables/PastEvent.java
@@ -1,43 +1,43 @@
-package tables;
-
-import java.util.Date; 
-
-public class PastEvent {
-    String eventName; 
-    int maxVolunteers; 
-    Date eventDate;
-    
-    public PastEvent(String name, int max, Date date){
-        this.eventName = name; 
-        this.maxVolunteers = max; 
-        this.eventDate = date; 
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public int getMaxVolunteers() {
-        return maxVolunteers;
-    }
-
-    public void setMaxVolunteers(int maxVolunteers) {
-        this.maxVolunteers = maxVolunteers;
-    }
-
-    public Date getEventDate() {
-        return eventDate;
-    }
-
-    public void setEventDate(Date eventDate) {
-        this.eventDate = eventDate;
-    }
-    
-    public String toString(){
-        return "Name: " + eventName + " Max Volunteers: " + maxVolunteers + " Date: " + eventDate; 
-    }
-}
+//package tables;
+//
+//import java.util.Date; 
+//
+//public class PastEvent {
+//    String eventName; 
+//    int maxVolunteers; 
+//    Date eventDate;
+//    
+//    public PastEvent(String name, int max, Date date){
+//        this.eventName = name; 
+//        this.maxVolunteers = max; 
+//        this.eventDate = date; 
+//    }
+//
+//    public String getEventName() {
+//        return eventName;
+//    }
+//
+//    public void setEventName(String eventName) {
+//        this.eventName = eventName;
+//    }
+//
+//    public int getMaxVolunteers() {
+//        return maxVolunteers;
+//    }
+//
+//    public void setMaxVolunteers(int maxVolunteers) {
+//        this.maxVolunteers = maxVolunteers;
+//    }
+//
+//    public Date getEventDate() {
+//        return eventDate;
+//    }
+//
+//    public void setEventDate(Date eventDate) {
+//        this.eventDate = eventDate;
+//    }
+//    
+//    public String toString(){
+//        return "Name: " + eventName + " Max Volunteers: " + maxVolunteers + " Date: " + eventDate; 
+//    }
+//}
