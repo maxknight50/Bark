5d4f790f0f3c5d24a5f7a9477548a5e2671d878b
mark <mark@10.211.55.3>
2022-06-24 17:20:11 -0400

    Modify Button for DonationList

--- src/bark/DonationList.java
+++ src/bark/DonationList.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
+import java.sql.Date; 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.collections.FXCollections;
@@ -41,10 +42,12 @@ public class DonationList extends Login1 {
     //Menu menuDonations = new Menu("Donations");
     //MenuItem enterDonations = new MenuItem("Enter Donations...");
 
+    Label donoID = new Label("Donation ID");
     Label donorName = new Label("Donor Name");
     Label donoAmount = new Label("Donation Amount");
     Label donoDate = new Label("Donation Date");
 
+    TextField idField = new TextField();
     TextField nameField = new TextField();
     TextField amountField = new TextField();
     TextField dateField = new TextField();
@@ -68,12 +71,14 @@ public class DonationList extends Login1 {
         table.add(populate, 0, 2);
 
         buttons.add(backBtn, 0, 0);
-        buttons.add(donorName, 0, 1);
-        buttons.add(nameField, 1, 1);
-        buttons.add(donoAmount, 0, 2);
-        buttons.add(amountField, 1, 2);
-        buttons.add(donoDate, 0, 3);
-        buttons.add(dateField, 1, 3);
+        buttons.add(donoID, 0, 1);
+        buttons.add(idField, 1, 1);
+        buttons.add(donorName, 0, 2);
+        buttons.add(nameField, 1, 2);
+        buttons.add(donoAmount, 0, 3);
+        buttons.add(amountField, 1, 3);
+        buttons.add(donoDate, 0, 4);
+        buttons.add(dateField, 1, 4);
         buttons.add(add, 0, 5);
         buttons.add(delete, 1, 5);
         buttons.add(modify, 0, 6);
@@ -86,7 +91,7 @@ public class DonationList extends Login1 {
 
         donTable.setItems(tableData);
 
-        id_col.setCellValueFactory(new PropertyValueFactory<Donation1, Integer>("donation_ID"));
+        id_col.setCellValueFactory(new PropertyValueFactory<Donation1, Integer>("donationID"));
         amountColumn.setCellValueFactory(new PropertyValueFactory<Donation1, String>("donationAmt"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Donation1, String>("donationName"));
         dateColumn.setCellValueFactory(new PropertyValueFactory<Donation1, Date>("donationDate"));
@@ -100,9 +105,10 @@ public class DonationList extends Login1 {
                 while (rs.next()) {
                     if (rs != null) {
                         donList.add(new Donation1(rs.getInt("donation_ID"), rs.getString("donationAmt"),
-                                rs.getString("donationName"), rs.getDate("donationDate"), rs.getInt("volID")));
+                                rs.getString("donationName"), rs.getString("donationDate"), rs.getInt("volID")));
                         break;
                     }
+                    System.out.println("ID: " + donList.get(i).getDonationID()); 
                 }
             }
             for (Donation1 x : donList) {
@@ -138,12 +144,37 @@ public class DonationList extends Login1 {
         });
 
         populate.setOnAction(e -> {
+            idField.setText(donTable.getSelectionModel().getSelectedItem().getDonationID() + ""); 
             nameField.setText(donTable.getSelectionModel().getSelectedItem().getDonationName());
             amountField.setText(donTable.getSelectionModel().getSelectedItem().getDonationAmt());
-            dateField.setText(donTable.getSelectionModel().getSelectedItem().getDonationDate().toString()); 
+            dateField.setText(donTable.getSelectionModel().getSelectedItem().getDonationDate());
         });
 
-    }
+        modify.setOnAction(e -> {
+            int newID = Integer.valueOf(idField.getText()); 
+            String newName = nameField.getText();
+            String newAmount = amountField.getText();
+            String newDate = dateField.getText();
+
+            String query = "UPDATE DONATION SET donationName = '" + newName + "', donationamt = '" + newAmount + "', donationdate = '" + newDate + "' WHERE donation_ID = '" + newID + "'";
+            sendDBCommand(query);
+            for (int i = 0; i < donList.size(); i++) {
+                if (donList.get(i).getDonationID() == newID) {
+                    donList.get(i).setDonationID(newID); 
+                    donList.get(i).setDonationAmt(newAmount);
+                    donList.get(i).setDonationName(newName);
+                    donList.get(i).setDonationDate(newDate); 
+                    break;
+                }
+            }
+            message.setText("Modify entry successful!");
+            tableData.clear();
+            for (Donation1 don : donList) {
+                tableData.add(don);
+            }
+        });
+
+ }
 
     public void sendDBCommand(String sqlQuery) {
         String URL = "jdbc:oracle:thin:@localhost:1521:XE";
